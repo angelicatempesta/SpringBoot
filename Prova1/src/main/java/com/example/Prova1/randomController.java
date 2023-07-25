45640fb9e9566d4c5bc7134be2a6ce1e4ae4d4e5
Angelica Tempesta <angelica.tempesta96@gmail.com>
2023-07-25 15:01:46 +0200

    esercizio 1.4

---
+++ Prova1/src/main/java/com/example/Prova1/randomController.java
@@ -0,0 +1,16 @@
+package com.example.Prova1;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class randomController {
+    @GetMapping("/random")
+    public ResponseEntity<?> randomResponse(){
+        return (new Random().nextBoolean() ? new ResponseEntity<>(HttpStatusCode.valueOf(200)) : new ResponseEntity<>(HttpStatusCode.valueOf(400)));
+    }
+}
