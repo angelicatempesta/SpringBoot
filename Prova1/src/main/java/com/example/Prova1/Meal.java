62391481ee81e56631c97342c97bb4f4d57afadb
Angelica Tempesta <angelica.tempesta96@gmail.com>
2023-07-25 15:34:57 +0200

    esercizio 2.2

--- Prova1/src/main/java/com/example/Prova1/Meal.java
+++ Prova1/src/main/java/com/example/Prova1/Meal.java
@@ -8,4 +8,28 @@ public class Meal {
     public Meal(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- Prova1/src/main/java/com/example/Prova1/mealController.java
+++ Prova1/src/main/java/com/example/Prova1/mealController.java
@@ -1,21 +1,41 @@
 package com.example.Prova1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 public class mealController {
     @GetMapping("/meals")
-    public List<Meal> mealList(){
+    public List<Meal> mealList() {
         List<Meal> mealList = new ArrayList<>();
-        mealList.add(new Meal ("Pasta al Pesto"));
-        mealList.add(new Meal ("Pennette all'Arrabbiata"));
-        mealList.add(new Meal ("Impepata di cozze"));
-        mealList.add(new Meal ("Pollo alla Piastra"));
+        mealList.add(new Meal("Pasta al Pesto"));
+        mealList.add(new Meal("Pennette all'Arrabbiata"));
+        mealList.add(new Meal("Impepata di cozze"));
+        mealList.add(new Meal("Pollo alla Piastra"));
 
         return mealList;
+    }
+
+
+    @GetMapping("/meal/{name}")
+    public Meal mealFunction(@PathVariable("name") String n) {
+        List<Meal> mealList = new ArrayList<>();
+        mealList.add(new Meal("Pasta al Pesto"));
+        mealList.add(new Meal("Pasta all'Arrabbiata"));
+        mealList.add(new Meal("Impepata di cozze"));
+        mealList.add(new Meal("Pollo alla Piastra"));
+
+        for (Meal m : mealList) {
+            if (m.getName().contains(n)) {
+              return m;
+            }
+        }
+       return null;
     }
 }
